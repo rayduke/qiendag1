b33f1a8673f859dd60fafb2eb22002d1eb95077d
Raymon den Hartogh <raydenhartogh@gmail.com>
2018-03-01 17:12:41 +0100

    final update

--- index.php
+++ index.php
@@ -1,12 +1,1 @@
-<?php echo "Hello World<br/>"; ?>
-
-<?php echo "test nog een test 2<br/>"; ?>
-
-<?php echo "this is nog een test<br/>"; ?>
-<?php echo "test<br/>"; ?>
-
-<?php function message(){
-    echo "Test bericht <br/>";
-}
-
-message(); ?>
+<a href="rekenmachine.php">test</a>
